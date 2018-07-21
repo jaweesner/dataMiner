428e8cc6b8a6632c643efbb77cd9a04f618f46f1
Jane Weesner <jane.e.weesner@gmail.com>
2018-07-21 01:07:59 -0700

    add bins and finished testing

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 
+//if not testing, call the main function
 if (!process.env.TESTENV){
   main(process.argv[2], process.argv[3], process.argv[4]);
 }
@@ -11,26 +12,24 @@ if (!process.env.TESTENV){
  * @param {String} file file path relative to run location of a json formatted file
  * @param {String} option type of filter to apply to json data
  * @param {String} query specific parameter to filter on 
+ * @returns {Array<Companies>} filtered array of companies
  */
 function main(file, option, query){
-  //ingest file
-  //readFileSync file to use even though blocking as rest of code depends on file contents
+  //readFileSync used as all other code depends on file contents
   let companies;
   try{
     companies = fs.readFileSync(path.resolve(file), 'utf8');
   } catch (e) {
     console.log('Error: File could not be read');
-    return;
+    return [];
   } try {
     companies = JSON.parse(companies);
   } catch (e) {
     console.log('Error: file is not properly formatted json');
-    return;
+    return [];
   }
 
   //define option handling
-  //type checking not needed as we are doing direct comparisons
-  //if wrong type, write out will write an appropriate message.
   
   optionRoutes = {
     locate: ({state}) => state === query,
@@ -40,7 +39,9 @@ function main(file, option, query){
     find_type: ({company_category}) => company_category === query
   }
   //in case the option is not in object, interpret the same as if query was invalid
-  writeOut(companies.filter(optionRoutes[option] || (()=>false) ));
+  const filteredArray = companies.filter(optionRoutes[option] || (()=>false));
+  writeOut(filteredArray);
+  return filteredArray;
 }
 
 /**
--- test/unit.test.js
+++ test/unit.test.js
@@ -17,16 +17,58 @@ describe('Console Output Function', ()=>{
   })
 })
 
-describe('Test Main CLI function', ()=>{
+describe('Test Main CLI function error handline', ()=>{
   
-  test('test error handling of incorrect files', (done)=>{
+  test('should handle incorrect files elegently', (done)=>{
     const mockConsoleLog = jest.spyOn(console, 'log');
-    main('notApath', 'locate', 'DC');
+    expect(main('notApath', 'locate', 'DC')).toEqual([]);
     expect(mockConsoleLog).toHaveBeenLastCalledWith('Error: File could not be read');
-    main('test/testData.js', 'locate', 'DC');
+    expect(main('test/testData.js', 'locate', 'DC')).toEqual([]);
     expect(mockConsoleLog).toHaveBeenLastCalledWith('Error: file is not properly formatted json');
+    mockConsoleLog.mockRestore();
     done();
   });
 
+  test('should not error for improper options and queries', (done)=>{
+    const mockConsoleLog = jest.spyOn(console, 'log');
+    expect(main('test/testData.json', 'locate', 12345)).toEqual([]);
+    expect(mockConsoleLog).toHaveBeenLastCalledWith('No companies found with given parameters');
+    expect(main('test/testData.json', 'notARoute', 'CA')).toEqual([]);
+    expect(mockConsoleLog).toHaveBeenLastCalledWith('No companies found with given parameters');
+    mockConsoleLog.mockRestore();
+    done();
+  })
 
+})
+
+describe('Test Main CLI function routes', () => {
+  test('should handle location filtering', (done) => {
+    expect(main('test/testData.json', 'locate', 'WI').map(({company_name})=>company_name)).toEqual(['BizVizz', 'JJ Keller', 'Locavore'])
+    expect(main('test/testData.json', 'locate', 'N/A')).toEqual([])
+    done();
+  })
+
+  test('should handle founded date - before filtering', (done) => {
+    expect(main('test/testData.json', 'find_before', '9')).toEqual([])
+    expect(main('test/testData.json', 'find_before', '1845').map(({company_name})=>company_name)).toEqual(['Citigroup', 'Deloitte', 'Dun & Bradstreet', 'J.P. Morgan Chase'])
+    expect(main('test/testData.json', 'find_before', 'ABC')).toEqual([])
+    done();
+  })
+  test('should handle founded date - after filtering', (done) => {
+    expect(main('test/testData.json', 'find_after', '100000')).toEqual([])
+    expect(main('test/testData.json', 'find_after', '2014').map(({company_name})=>company_name)).toEqual(['48 Factoring Inc.', 'BaleFire Global', 'Compared Care', 'CONNECT-DOT LLC.', "How's My Offer?", "Kimono Labs"])
+    expect(main('test/testData.json', 'find_after', '%')).toEqual([])
+    done();
+  })
+
+  test('should handle company-size filtering', (done) => {
+    expect(main('test/testData.json', 'find_companies_between_size', '201-500').map(({company_name})=>company_name)).toEqual(["AccuWeather","Alarm.com","Arpin Van Lines","Atlas Van Lines","Business Monitor International","CAN Capital","Certara","Copyright Clearance Center","DataLogix","Evidera","GitHub","Impaq International","Inrix Traffic","LoopNet","MicroBilt Corporation"])
+    expect(main('test/testData.json', 'find_companies_between_size', '8-15')).toEqual([]);
+    done();
+  })
+  test('should handle company-type filtering', (done) => {
+    expect(main('test/testData.json', 'find_type', 'Not A Type').toEqual([]);
+    expect(main('test/testData.json', 'find_type', 'Food & Agriculture').map(({company_name})=>company_name)).toEqual(["Climate Corporation", "FarmLogs", "Food+Tech Connect", "Locavore", "Mercaris"])
+    done();
+  })
 })
